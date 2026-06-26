e55eebd322aca12e135d281d9e5f7be3b50fc5e4
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-06-25 23:28:50 -0400

    working on adding friction and acceleration to the polayermovment

--- systems/state_machine/states/move.gd
+++ systems/state_machine/states/move.gd
@@ -1,35 +1,43 @@
 class_name Move extends State
 
-@export var speed : float = 8.0
+@export var speed : float = 15.0
+@export var acceleration: float = 30.0
+@export var friction: float = 10.0
 @export var look_angle_speed : float = 10.0
-@export var body: RigidBody3D
+@export var entity: RigidBody3D
 
 var direction: Vector2
 
 func enter() -> void:
-	if body == null:
-		print_debug("The Move node needs a body assigned to it")
+	if entity == null:
+		print_debug("The Move node needs a entity assigned to it")
 		return
 		
 func physics_update(delta: float) -> void:
 	
-	if body.input_controller.jump_pressed and body.ground_detection.is_grounded():
+	if entity.input_controller.jump_pressed and entity.ground_detection.is_grounded():
 		change_state.emit(self, "jump")
 		return
 		
-	if body.input_controller.dash_pressed and body.ground_detection.is_grounded():
+	if entity.input_controller.dash_pressed and entity.ground_detection.is_grounded():
 		change_state.emit(self, "dash")
 		return
 	
 	# Directional Movement	
-	direction = body.input_controller.move_direction.rotated(-body.camera_controller.spring_arm.global_rotation.y)
+	direction = entity.input_controller.move_direction.rotated(-entity.camera_controller.spring_arm.global_rotation.y)
 	
-	body.linear_velocity.x = direction.x * speed
-	body.linear_velocity.z = direction.y * speed
-	
+	entity.linear_velocity.x = direction.x 
+	entity.linear_velocity.z = direction.y  
+		
+	if entity.linear_velocity.length() > 0:
+		
+		entity.linear_velocity.move_toward(Vector3(direction.x, 0, direction.y * speed) , acceleration)
+	else:
+		
+		entity.linear_velocity.move_toward(Vector3.ZERO, friction)
 
 
 # Look Direction
 	if direction.length() > 0.0:
 		var look_angle = -direction.angle() + PI/2
-		body.model.rotation.y = rotate_toward(body.model.rotation.y, look_angle, look_angle_speed * delta)
+		entity.model.rotation.y = rotate_toward(entity.model.rotation.y, look_angle, look_angle_speed * delta)
