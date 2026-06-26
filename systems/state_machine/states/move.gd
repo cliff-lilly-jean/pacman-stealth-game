2b788d13841cb6c7f855cfaea28cf2e5289eeecf
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-06-26 14:59:18 -0400

    added friction and acceleration to the mve ment of the playert

--- systems/state_machine/states/move.gd
+++ systems/state_machine/states/move.gd
@@ -1,12 +1,15 @@
 class_name Move extends State
 
-@export var speed : float = 1200.0
-@export var acceleration: float = 2.0
-@export var friction: float = 0.01
+@export var speed : float = 10.0
+@export var acceleration: float = 5.0
+@export var friction: float = 0.5
 @export var look_angle_speed : float = 10.0
 @export var entity: RigidBody3D
 
 var direction: Vector2
+
+func _ready() -> void:
+	entity.physics_material_override.friction = friction
 
 func enter() -> void:
 	if entity == null:
@@ -25,18 +28,14 @@ func physics_update(delta: float) -> void:
 	
 	# Directional Movement	
 	direction = entity.input_controller.move_direction.rotated(-entity.camera_controller.spring_arm.global_rotation.y)
+	direction.normalized()
 	
-	entity.linear_velocity.x = direction.x 
-	entity.linear_velocity.z = direction.y  
-		
 	if direction.length() > 0.1:
-		
-		entity.linear_velocity.move_toward(Vector3(direction.x, 0, direction.y * speed) , acceleration)
-		
+		entity.linear_velocity.x = move_toward(entity.linear_velocity.x, direction.x * speed, acceleration) 
+		entity.linear_velocity.z = move_toward(entity.linear_velocity.z, direction.y * speed, acceleration)
 	else:
-		
-		entity.linear_velocity.move_toward(Vector3.ZERO, friction)
-
+		entity.linear_velocity.x = move_toward(entity.linear_velocity.x, direction.x * speed, friction) 
+		entity.linear_velocity.z = move_toward(entity.linear_velocity.z, direction.y * speed, friction)
 
 # Look Direction
 	if direction.length() > 0.0:
