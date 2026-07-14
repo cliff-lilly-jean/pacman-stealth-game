fef02bc1203671064735fa093b23224dad985cec
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-07-14 19:58:00 -0400

    updaed the gamemanager and the coinspellets signals

--- scenes/ui/score/score.gd
+++ scenes/ui/score/score.gd
@@ -1,3 +1,14 @@
 extends Label
 
-	
+var score: int
+
+signal update_score
+
+func _ready() -> void:
+	update_score.connect(on_update_score)
+
+func _process(delta: float) -> void:
+	text = "Score: " + str(score)
+
+func on_update_score() -> void:
+	score += 1
--- scenes/world/game_manager.gd
+++ scenes/world/game_manager.gd
@@ -4,13 +4,12 @@ extends Node
 @onready var pellets = %Pellets
 @onready var score = %Score
 
-var current_score: int = 0
-
 func _ready() -> void:
 	for pellet in pellets.get_children():
 		pellet.collected.connect(on_collected)
-
+	
+	
 
 func on_collected() -> void:
-	current_score += 1
-	score.text = str(current_score)
+	score.update_score.emit()
+	
