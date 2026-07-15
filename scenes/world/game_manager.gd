cd5bc817233624fdd30519440398be40b784079b
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-07-15 17:03:37 -0400

    game manager updates

--- scenes/world/game_manager.gd
+++ scenes/world/game_manager.gd
@@ -4,12 +4,18 @@ extends Node
 @onready var pellets = %Pellets
 @onready var score = %Score
 
+var game_clock: int
+
+
 func _ready() -> void:
 	for pellet in pellets.get_children():
 		score.total = pellets.get_child_count()
 		pellet.collected.connect(on_collected)
 	
-	
+
+func _process(delta: float) -> void:
+	game_clock += 1 * delta
+	print(game_clock)
 
 func on_collected() -> void:
 	score.update_score.emit()
