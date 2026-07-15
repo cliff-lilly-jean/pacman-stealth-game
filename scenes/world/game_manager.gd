db1fc1d3eeca6af111c097f2b393283e0b5c43d8
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-07-15 17:22:38 -0400

    added a game time to keep track of how long it takes to collect all the pellets

--- scenes/world/game_manager.gd
+++ scenes/world/game_manager.gd
@@ -3,20 +3,28 @@ extends Node
 @onready var player = %Player
 @onready var pellets = %Pellets
 @onready var score = %Score
+@onready var game_timer: Timer = %GameTimer
+@onready var game_time: Label = %GameTime
 
-var game_clock: int
 
+
+var time_in_seconds: int = 0
 
 func _ready() -> void:
+	## Pellets
 	for pellet in pellets.get_children():
 		score.total = pellets.get_child_count()
 		pellet.collected.connect(on_collected)
-	
-
-func _process(delta: float) -> void:
-	game_clock += 1 * delta
-	print(game_clock)
+		
+	## Game timer
+	game_timer.timeout.connect(on_timer_timeout)
 
 func on_collected() -> void:
 	score.update_score.emit()
 	
+
+func on_timer_timeout() -> void:
+	time_in_seconds += 1
+	var minutes = int(time_in_seconds / 60)
+	var seconds = time_in_seconds - minutes * 60
+	game_time.text = "%02d:%02d" % [minutes, seconds]
