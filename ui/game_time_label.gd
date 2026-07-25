7fc131733f235c85344c433dc0c79a06deac01fb
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-07-25 07:44:14 -0400

    fixed the countdown timer

--- ui/game_time_label.gd
+++ ui/game_time_label.gd
@@ -3,14 +3,21 @@ extends Label
 @export var game_time_in_seconds: float
 @onready var timer: Timer = $Timer
 
+var minutes: int
+var seconds: float
+
 func _ready() -> void:
 	timer.timeout.connect(on_timer_timeout)
+	
+	timer.start()
+
+func _process(_delta: float) -> void:
+	minutes = int(game_time_in_seconds / 60)
+	seconds = game_time_in_seconds - minutes * 60
+	text = "%02d:%02d" % [minutes, seconds]
 
 func on_timer_timeout() -> void:
 	game_time_in_seconds -= 1
-	var minutes = int(game_time_in_seconds / 60)
-	var seconds = game_time_in_seconds - minutes * 60
-	text = "%02d:%02d" % [minutes, seconds]
 	
 	if game_time_in_seconds == 0:
 		print("Times up")
