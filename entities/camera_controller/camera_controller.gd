51c8c1c7c877076e31118562456dc22ba6dfe173
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-08-02 11:02:06 -0400

    minor camera updates

--- entities/camera_controller/camera_controller.gd
+++ entities/camera_controller/camera_controller.gd
@@ -1,7 +1,7 @@
 class_name CameraController extends SpringArm3D
 
 const max_look_down_angle: float = deg_to_rad(-15)
-const max_look_up_angle: float = deg_to_rad(-45)
+const max_look_up_angle: float = deg_to_rad(-40)
 
 @export var mouse_sensitivity: float
 @export var joystick_sensitivity: float
