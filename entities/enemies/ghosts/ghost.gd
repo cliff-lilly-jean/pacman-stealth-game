169a9f5664ff8812a9908e603b171e68068eb4be
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-08-06 18:37:41 -0400

    minor updates to the enemey class

--- entities/enemies/ghosts/ghost.gd
+++ entities/enemies/ghosts/ghost.gd
@@ -1,5 +1,14 @@
 class_name Ghost extends CharacterBody3D
 
+@export var patrol_distance_length: float
+
+@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
+@onready var deection_area: DetectionArea = $DeectionArea
+@onready var look_direction: RayCast3D = $LookDirection
+@onready var mesh: MeshInstance3D = $MeshInstance3D
+
+var start_patrol_locaton: Vector3
+var end_patrol_location: Vector3
 
 ## Patrol
 ## Set the target position
@@ -21,3 +30,21 @@ class_name Ghost extends CharacterBody3D
 ## Set the target position, the Player
 ## Get the next path position
 ## Move toward he next position
+
+func _ready() -> void:
+	set_navigation_target_position()
+	
+
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func set_navigation_target_position() -> void:
+	start_patrol_locaton = Vector3(global_position.x, 0, global_position.z)
+	end_patrol_location = Vector3(randf_range(start_patrol_locaton.x, patrol_distance_length), 0, randf_range(start_patrol_locaton.z, patrol_distance_length))
+	
+	navigation_agent.target_position = start_patrol_locaton
+	print("Nav start location: ", navigation_agent.target_position)
+	
