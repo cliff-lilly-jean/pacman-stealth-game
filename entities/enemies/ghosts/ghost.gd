87013481c054d64664c79bc954b43340e34086c2
Cliff Jean <cliff.lilly.jean@gmail.com>
2026-08-17 07:07:09 -0400

    created a patrol ai state

--- entities/enemies/ghosts/ghost.gd
+++ entities/enemies/ghosts/ghost.gd
@@ -1,19 +1,16 @@
-class_name Ghost extends CharacterBody3D
+class_name Ghost extends Enemy
 
 @export var speed: float
 @export var patrol_distance_length: float
 @export var look_rotation_speed: float
 @onready var body: MeshInstance3D = $Body
 
-@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 @onready var detection_area: DetectionArea = $DetectionArea
 
-
 ## Patrol Variables
-var start_patrol_locaton: Vector3
-var end_patrol_location: Vector3
+@onready var start_patrol_locaton: Vector3 = global_position
+@onready var end_patrol_location: Vector3 = Vector3(randf_range(start_patrol_locaton.x - patrol_distance_length, start_patrol_locaton.x + patrol_distance_length), global_position.y, randf_range(start_patrol_locaton.z - patrol_distance_length, start_patrol_locaton.z + patrol_distance_length))
 var next_position: Vector3
-var moving_to_end:bool = true
 
 
 ## CHASE
@@ -26,7 +23,6 @@ func _ready() -> void:
 	set_navigation_route()
 	
 func _physics_process(delta: float) -> void:
-	patrol()
 	
 	
 	look_rotation(delta)
@@ -36,24 +32,7 @@ func set_navigation_route() -> void:
 	start_patrol_locaton = global_position
 	end_patrol_location = Vector3(randf_range(start_patrol_locaton.x - patrol_distance_length, start_patrol_locaton.x + patrol_distance_length), global_position.y, randf_range(start_patrol_locaton.z - patrol_distance_length, start_patrol_locaton.z + patrol_distance_length))
 	
-	navigation_agent.target_position = end_patrol_location
-
-## Patrol
-func patrol() -> void: 
-	next_position = navigation_agent.get_next_path_position()
-	
-	var direction: Vector3 = global_position.direction_to(next_position)
-	velocity = direction * speed
-	
-	if navigation_agent.is_navigation_finished():
-		## Determine if the ghost has finished the patrol to the edn point, if so change the route to the starting point  and travel to that
-		if moving_to_end:
-			navigation_agent.target_position = start_patrol_locaton
-			next_position = navigation_agent.get_next_path_position()
-			moving_to_end = false
-		else:
-			navigation_agent.target_position = end_patrol_location
-			moving_to_end = true
+	nav_agent.target_position = end_patrol_location
 
 
 func investigate() -> void:
---
+++ entities/enemies/ghosts/states/patrol_ai.gd
@@ -0,0 +1,23 @@
+extends State
+class_name PatrolAI
+
+@export var enemy: Enemy
+
+var moving_to_end:bool = true
+
+
+func physics_update(delta: float) -> void:
+	enemy.next_position = enemy.nav_agent.get_next_path_position()
+	
+	var direction: Vector3 = enemy.global_position.direction_to(enemy.next_position)
+	enemy.velocity = direction * enemy.speed
+	
+	if enemy.nav_agent.is_navigation_finished():
+		## Determine if the ghost has finished the patrol to the edn point, if so change the route to the starting point  and travel to that
+		if moving_to_end:
+			enemy.nav_agent.target_position = enemy.start_patrol_locaton
+			enemy.next_position = enemy.nav_agent.get_next_path_position()
+			moving_to_end = false
+		else:
+			enemy.nav_agent.target_position = enemy.end_patrol_location
+			moving_to_end = true
